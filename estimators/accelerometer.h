763463ab4a1941d0f3ade66c93e7e00bf9ef377f
Mauro Mombelli <mauro@mombelli.me>
2015-06-07 00:21:06 +0200

    fixed import path

--- estimators/accelerometer.h
+++ estimators/accelerometer.h
@@ -1,7 +1,7 @@
 #ifndef ACCELEROMETER_H_
 #define ACCELEROMETER_H_
 
-#include "my_math/my_math.h"
+#include "TrigonomeC/my_math.h"
 
 void get_estimated_error_acce(struct Quaternion4f q, struct Vector3f *ris, struct Vector3f rawData);
 
--- estimators/magnetometer.h
+++ estimators/magnetometer.h
@@ -1,7 +1,7 @@
 #ifndef MAGNETOMETER_H_
 #define MAGNETOMETER_H_
 
-#include "my_math/my_math.h"
+#include "TrigonomeC/my_math.h"
 
 void get_estimated_error_magne(struct Quaternion4f q, struct Vector3f *ris, struct Vector3f rawData);
 
